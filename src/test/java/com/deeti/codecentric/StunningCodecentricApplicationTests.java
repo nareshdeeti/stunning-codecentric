ee513b4a266b52fd5c123c4b5c0783af09f55094
Naresh Deeti <nareshdeeti@hotmail.com>
2023-02-14 23:18:10 +0530

    Initial commit

---
+++ src/test/java/com/deeti/codecentric/StunningCodecentricApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deeti.codecentric;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StunningCodecentricApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
